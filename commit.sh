f1df1c2689c0206c59aaf6ef6a244fad1d23b250
root <root@tx-k8s01.data>
2019-06-24 16:16:23 +0800

    new

--- commit.sh
+++ commit.sh
@@ -1,13 +1,20 @@
 #! /bin/bash
 
+if [[ $# != 1]];
+then
+  echo "enter input images"
+  exit 1
+if
+
 image=${1}
 tmp_image=${image#*\/}
 gen_image=${tmp_image//[\/:]/-}
 
+
 git checkout .
 git pull 
 echo "From ${image}" > Dockerfile
-echo ">>>>>>>>>>>>>>>>>Dockerfile has changed to :" cat Dockerfile
+echo ">>>>>>>>>>>>>>>>>Dockerfile has changed to ${image}:"
 echo -e ">>>>>>>>>>>>>>>>>start to commit change for image: "${image}"\n"
 git add .
 git commit -m "add images '${image}'"
@@ -15,7 +22,5 @@ git tag release-v${gen_image}
 git push --tags
 echo -e ">>>>>>>>>>>>>>>>>commit successfully!\n"
 echo -e "You can download image by following command:\n"
-echo -e "./download.sh ${image}\n"
-echo "*************************************************"
-echo "If download failed after a long time, please submit a issue at: https://github.com/soolaugust/containers/issues"
+echo -e "./download.sh ${image} tx-k8s10.data/images\n"
 echo "*************************************************"
---
+++ pull.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+image=${1}
+tmp_image=${image#*\/}
+gen_image=${tmp_image//[\/:]/-}
+image_local=${2}
+
+
+registry_address="registry.cn-beijing.aliyuncs.com"
+aliyun_image="/kubeflow-xxf/pull-images"
+version=${gen_image}
+
+
+if [ $# = 2 ];
+then
+  #image=${1}
+  #image_local=${2}
+  echo "use default registry"
+elif [ $# = 4 ];
+then
+  #image=${1}
+  #image_local=${1}
+  registry_address=${3}
+  aliyun_image=${4}
+  echo "change registry address to ${registry_address}${aliyun_image}"
+else
+  echo "invalid args number, cmd image_local reg_address aliyun_images"
+  exit 1
+fi
+
+
+echo "will download this image from: ${registry_address}${aliyun_image}"
+docker pull ${registry_address}${aliyun_image}:&{version}
+docker tag  ${registry_address}${aliyun_image}:&{version} ${image_local}
+docker rmi ${registry_address}${aliyun_image}:&{version}
+echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> download successfully!\n
